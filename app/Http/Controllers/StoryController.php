80946fea7ddbf59671921d2f81397180b44a1ac7
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-04-26 14:56:58 +0530

    fix

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -22,7 +22,7 @@ class StoryController extends Controller
     {
         $cat_name = str_replace('-', ' ', $name);
         $category = Category::where('eng_name', $cat_name)->first();
-        $blog = Blog::where('categories_ids', $category->id)->with('images')->paginate(1);
+        $blog = Blog::where('categories_ids', $category->id)->with('images')->paginate(10);
         return view('category',['category'=> $category,'blogs' => $blog]);
     }
 }
