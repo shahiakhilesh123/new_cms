3453a700d97644ad997805fd41883444f84d0983
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-04-27 12:16:13 +0530

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -921,7 +921,7 @@
                     <div id="main-nav" class="primary-navigation">
                         <ul id="menu-main-menu" class>
                             <li class="home-btn"><a href="{{ asset('/') }}"><i
-                                        class="feather icon-home" aria-hidden="true"></i></a></li>
+                                        class="fa fa-home" aria-hidden="true"></i></a></li>
                             <?php 
                             $menus = App\Models\Menu::whereHas('type', function ($query) {
                                 $query->where('type', 'Header');
