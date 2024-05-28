e6bd73f933d11e7f27f06b156434a008d031d513
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-05-28 11:51:25 +0530

    fix

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -264,11 +264,11 @@
                                                         </div><a target="__blank"
                                                             href="https://www.youtube.com/@SportsHour"><img
                                                                 width="400" height="300"
-                                                                src="{{ asset('/banner/Website_Sports.jpeg') }}"
+                                                                src="{{ asset('/banner/Website_Sports.jpg') }}"
                                                                 class="image wp-image-709  attachment-full size-full"
                                                                 alt="" style="max-width: 100%; height: auto;"
                                                                 decoding="async" loading="lazy"
-                                                                srcset="{{ asset('/banner/Website_Sports.jpeg') }}"
+                                                                srcset="{{ asset('/banner/Website_Sports.jpg') }}"
                                                                 sizes="(max-width: 400px) 100vw, 400px"></a>
                                                     </div>
                                                 </aside>
