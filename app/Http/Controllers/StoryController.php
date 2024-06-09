e62fcb6356d1213e19c8b0732025d2be4438f0fe
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-06-09 22:19:09 +0530

    fix

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -11,7 +11,7 @@ class StoryController extends Controller
     public function showStory($cat_name, $name)
     {
         //echo $blog_name = str_replace('-', ' ', $name);
-        $blog = Blog::where('site_url', $name)->with('images')->first();
+        $blog = Blog::where('id', 10)->with('images')->first();
         $author =  User::where('id', $blog->author)->first();
         $category = Category::where('id', $blog->categories_ids)->first();
         $other_blog = Blog::where('id','!=', $blog->id)->with('thumbnail')->where('categories_ids', $blog->categories_ids)->with('images')->limit(6)->get()->all();
@@ -50,8 +50,18 @@ class StoryController extends Controller
         $search = isset($_REQUEST['search']) ? $_REQUEST['search'] : '';
         $count = 10;
         //$category = Category::where('site_url', $name)->first();
-        $blog = Blog::where('name', 'like', '%' . $search . '%')->where('status', 1)->with('images')->orderBy('created_at', 'DESC')->paginate($count);
+        $blog = Blog::where('name', 'like', '%' . $search . '%')->orWhere('tags', 'like', '%' . $search . '%')->where('status', 1)->with('images')->orderBy('created_at', 'DESC')->paginate($count);
         $blog->setPath(asset('/search').'?search='.$search);
         return view('search',['blogs' => $blog, 'search' => $search, 'page' => $page, 'count' => $count]);
+    }
+    public function author($name)
+    {
+        $page = isset($_REQUEST['page']) ? $_REQUEST['page'] : 1;
+        $count = 10;
+        $name = str_replace('_', ' ', $name);
+        $user  = User::where('url_name', $name)->first();
+        $blog = Blog::where('author', $user->id)->where('status', 1)->with('images')->orderBy('id', 'DESC')->paginate($count);
+        $blog->setPath(asset('/').$name);
+        return view('author',['users'=> $user,'blogs' => $blog, 'page' => $page, 'count' => $count]);
     }
 }
