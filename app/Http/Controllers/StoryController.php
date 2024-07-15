cc78924972c248ec9da191a2b27fac7a6f75790b
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-07-15 22:06:10 +0530

    fix

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -16,7 +16,7 @@ class StoryController extends Controller
         $author =  User::where('id', $blog->author)->first();
         $category = Category::where('id', $blog->categories_ids)->first();
         $other_blog = Blog::where('id','!=', $blog->id)->with('thumbnail')->where('categories_ids', $blog->categories_ids)->with('images')->limit(6)->get()->all();
-        $latests = Blog::where('id','!=', $blog->id)->whereNull('link')->with('thumbnail')->with('images')->orderBy('created_at', 'DESC')->limit(6)->get()->all();
+        $latests = Blog::where('categories_ids', $category->id)->where('id','!=', $blog->id)->whereNull('link')->with('thumbnail')->with('images')->orderBy('created_at', 'DESC')->limit(6)->get()->all();
         //$videos_latests = Blog::where('id','!=', $blog->id)->whereNotNull('link')->with('thumbnail')->with('images')->orderBy('id', 'DESC')->limit(6)->get()->all();
         return view('detail')->with('data', ['blog'=> $blog, 'relates'=> $other_blog, 'latests' => $latests, 'category' => $category, 'author' => $author]);
     }
