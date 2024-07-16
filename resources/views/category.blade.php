40f701f5b29e60bffdd68f3d8cbb6187179b330a
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-07-16 07:42:18 +0530

    fix

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -214,10 +214,17 @@
                                                             </section>
                                                         </div>
                                                     </section>
-                                                    <div class="pagination float-left" style="text-align: end;">
+                                                    <div class="pagination float-right col-md-6">
+                                                            <!-- <div class="left"> -->
+                                                                @if($page <= 4 && count($blogs) == $count && $page != 1)
+                                                                <a style="color: #1da1f2;" href="{{ asset('/') }}{{ isset($category->site_url) ? $category->site_url : '' }}?page={{ $page -1}}">पीछे&nbsp;&nbsp;<i class="fa fa-angle-double-right"></i></a>
+                                                                @endif
+                                                                <!-- <div> -->
+                                                    </div>
+                                                    <div class="pagination float-left col-md-6" style="text-align: end;">
                                                             <!-- <div class="left"> -->
                                                                 @if($page <= 4 && count($blogs) == $count)
-                                                                <a style="color: #1da1f2;" href="{{ asset('/') }}{{ isset($category->site_url) ? $category->site_url : '' }}?page={{ $page +1}}">Read more&nbsp;&nbsp;<i class="fa fa-angle-double-right"></i></a>
+                                                                <a style="color: #1da1f2;" href="{{ asset('/') }}{{ isset($category->site_url) ? $category->site_url : '' }}?page={{ $page +1}}">आगे&nbsp;&nbsp;<i class="fa fa-angle-double-right"></i></a>
                                                                 @endif
                                                                 <!-- <div> -->
                                                     </div>
