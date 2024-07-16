6037c20ecd792641ac7ce612c75fcac87f3d852d
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-07-16 07:56:58 +0530

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -522,7 +522,6 @@
                     <ul class="mobile-inline-link">
                     <li class="home-btn"><a href="{{ asset('/') }}"><i class="fa fa-home" aria-hidden="true"></i></a></li>
                     <li class="home-btn"><a href="{{ asset('/news') }}">न्यूज</a></li>
-                    <li class="home-btn"><a href="{{ asset('/election-2024') }}">चुनाव 2024</a></li>
                     <li class="home-btn"><a href="{{ asset('/dharma-gyan') }}">धर्म ज्ञान</a></li>
                     <!-- <li class="home-btn"><a href="{{ asset('/exclusive') }}">एक्सक्लूसिव </a></li> -->
                     <li class="home-btn"><a href="{{ asset('/program') }}">कड़क बात </a></li>
