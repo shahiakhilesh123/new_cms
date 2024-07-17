d9bca0a260113e9fa6d466e89867de55e643304f
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-07-17 14:38:52 +0530

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -461,6 +461,7 @@
                         <div class="cm-col-lg-8 cm-col-12">
                             <div class="advertisement-area">
                                 <!-- Newsnmf_Header728 -->
+                           <!-- HeaderResponsive_728 -->
                             <ins class="adsbygoogle"
                                 style="display:block"
                                 data-ad-client="ca-pub-3986924419662120"
