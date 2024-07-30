b56d8c79e8e9ecf5b7334a230718aa0ecb29bf9e
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-07-30 12:47:32 +0530

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -923,17 +923,31 @@
                                             $truncated = $symbol.$blog->name;
                                             $ff = isset($blog_file->file_name) ? $blog_file->file_name : ''; 
                                             ?>
-                                            <li class="cat-item cat-item-16"><a
-                                                    href="{{ asset('/') }}{{isset($cat->site_url) ? $cat->site_url : ''}}/{{  $blog->site_url }}">
+                                            <li class="cat-item cat-item-16">
                                                     <div class="row">
-                                                        <div class="cm-col-lg-3 cm-col-md-3 cm-col-2">
-                                                        <img width="100" height="100" src="{{ asset('/file').'/'.$ff }}" alt="{{ $blog->name }}"
-                                                        decoding="async" />
+                                                    <div class="cm-col-lg-5 cm-col-md-5 cm-col-4">
+                                                        <div class="post_thumb">
+                                                            <a
+                                                                href="{{ asset('/') }}{{  isset($cat->site_url) ? $cat->site_url : '' }}/<?php echo isset($blog->site_url) ? $blog->site_url : ''; ?>">
+                                                                <figure class="imghover">
+                                                                    <img width="720" height="540"
+                                                                        src="{{ asset('/file').'/'.$ff }}"
+                                                                        class="attachment-cream-magazine-thumbnail-3 size-cream-magazine-thumbnail-3 wp-post-image"
+                                                                        alt="{{ $blog->name }}"
+                                                                        decoding="async" />
+                                                                </figure>
+                                                            </a>
                                                         </div>
-                                                        <div class="cm-col-lg-9 cm-col-md-9 cm-col-10"><?php echo $truncated; ?></div>
                                                     </div>
-                                                    </a>
-                                                
+                                                    <div class="cm-col-lg-7 cm-col-md-7 cm-col-8">
+                                                        <div class="right_box">
+                                                            <div class="post_title">
+                                                                <h2><a
+                                                                        href="{{ asset('/') }}{{  isset($cat->site_url) ? $cat->site_url : '' }}/<?php echo isset($blog->site_url) ? $blog->site_url : ''; ?>"><?php echo $truncated; ?></a></h2>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </li>
                                             @endforeach
                                         </ul>
@@ -960,16 +974,30 @@
                                             $truncated = $symbol.$blog->name;
                                             $ff = isset($blog_file->file_name) ? $blog_file->file_name : ''; 
                                             ?>
-                                            <li class="cat-item cat-item-16"><a
-                                                    href="{{ asset('/') }}{{isset($cat->site_url) ? $cat->site_url : ''}}/{{  $blog->site_url }}">
-                                                    <div class="row">
-                                                        <div class="cm-col-lg-3 cm-col-md-3 cm-col-2">
-                                                        <img width="100" height="100" src="{{ asset('/file').'/'.$ff }}" alt="{{ $blog->name }}"
-                                                        decoding="async" />
+                                            <li class="cat-item cat-item-16"><div class="row">
+                                                    <div class="cm-col-lg-5 cm-col-md-5 cm-col-4">
+                                                        <div class="post_thumb">
+                                                            <a
+                                                                href="{{ asset('/') }}{{  isset($cat->site_url) ? $cat->site_url : '' }}/<?php echo isset($blog->site_url) ? $blog->site_url : ''; ?>">
+                                                                <figure class="imghover">
+                                                                    <img width="720" height="540"
+                                                                        src="{{ asset('/file').'/'.$ff }}"
+                                                                        class="attachment-cream-magazine-thumbnail-3 size-cream-magazine-thumbnail-3 wp-post-image"
+                                                                        alt="{{ $blog->name }}"
+                                                                        decoding="async" />
+                                                                </figure>
+                                                            </a>
                                                         </div>
-                                                        <div class="cm-col-lg-9 cm-col-md-9 cm-col-10"><?php echo $truncated; ?></div>
                                                     </div>
-                                                </a>
+                                                    <div class="cm-col-lg-7 cm-col-md-7 cm-col-8">
+                                                        <div class="right_box">
+                                                            <div class="post_title">
+                                                                <h2><a
+                                                                        href="{{ asset('/') }}{{  isset($cat->site_url) ? $cat->site_url : '' }}/<?php echo isset($blog->site_url) ? $blog->site_url : ''; ?>"><?php echo $truncated; ?></a></h2>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
                                                 
                                             </li>
                                             @endforeach
