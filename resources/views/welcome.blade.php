8aaafe28c07f31f432de4aec16d328c7f8fea2b7
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-07-30 12:24:05 +0530

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -927,7 +927,7 @@
                                                     href="{{ asset('/') }}{{isset($cat->site_url) ? $cat->site_url : ''}}/{{  $blog->site_url }}">
                                                     <div class="row">
                                                         <div class="cm-col-lg-3 cm-col-md-3 cm-col-2">
-                                                        <img width="50" height="100" src="{{ asset('/file').'/'.$ff }}" alt="{{ $blog->name }}"
+                                                        <img width="100" height="100" src="{{ asset('/file').'/'.$ff }}" alt="{{ $blog->name }}"
                                                         decoding="async" />
                                                         </div>
                                                         <div class="cm-col-lg-9 cm-col-md-9 cm-col-10"><?php echo $truncated; ?></div>
@@ -964,7 +964,7 @@
                                                     href="{{ asset('/') }}{{isset($cat->site_url) ? $cat->site_url : ''}}/{{  $blog->site_url }}">
                                                     <div class="row">
                                                         <div class="cm-col-lg-3 cm-col-md-3 cm-col-2">
-                                                        <img width="50" height="100" src="{{ asset('/file').'/'.$ff }}" alt="{{ $blog->name }}"
+                                                        <img width="100" height="100" src="{{ asset('/file').'/'.$ff }}" alt="{{ $blog->name }}"
                                                         decoding="async" />
                                                         </div>
                                                         <div class="cm-col-lg-9 cm-col-md-9 cm-col-10"><?php echo $truncated; ?></div>
