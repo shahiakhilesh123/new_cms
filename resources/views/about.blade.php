ed207dd77cb712f85b52d534eba746cf93a0730c
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-07-30 09:41:05 +0530

    fix

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,13 +10,13 @@
                             
                             <h5>About Us</h5>
                             <h5>NMF NEWS: Unveiling the Truth Behind the Headlines</h5>
-                            <p>NMF NEWS breaks the mould of conventional news reporting by providing in-depth analysis and the "real face" of every story, all accessible with a single click. Dive into the latest news across India and the globe, encompassing politics, business, technology, entertainment, sports, science, and more. We offer a comprehensive perspective on the world around you.</p>
+                            <p style="font-weight: 300;">NMF NEWS breaks the mould of conventional news reporting by providing in-depth analysis and the "real face" of every story, all accessible with a single click. Dive into the latest news across India and the globe, encompassing politics, business, technology, entertainment, sports, science, and more. We offer a comprehensive perspective on the world around you.</p>
                             <h5>Personalized Content, Trusted Source</h5>
-                            <p>Our content is personalized to your interests, ensuring you receive the news that matters most to you. NMF NEWS is a copyright-protected platform, meticulously curated by Khetan Media Creation Pvt Ltd.</p>
+                            <p style="font-weight: 300;">Our content is personalized to your interests, ensuring you receive the news that matters most to you. NMF NEWS is a copyright-protected platform, meticulously curated by Khetan Media Creation Pvt Ltd.</p>
                             <h5>Driven by Passion and Expertise</h5>
-                            <p>Founded in 2015 by Parmanand Khetan, a visionary entrepreneur with a deep understanding of the media landscape, Khetan Media Creations is driven by a relentless pursuit of excellence. We invite you to explore our world of creative content, meticulously crafted by our dedicated team.</p>
+                            <p style="font-weight: 300;">Founded in 2015 by Parmanand Khetan, a visionary entrepreneur with a deep understanding of the media landscape, Khetan Media Creations is driven by a relentless pursuit of excellence. We invite you to explore our world of creative content, meticulously crafted by our dedicated team.</p>
                             <h5>Parmanand Khetan: The Guiding Force</h5>
-                            <p>Parmanand Khetan is a respected business leader, frequently sought after for his media expertise. He leads by example, consistently exceeding expectations and inspiring his team to do the same. While approachable and easygoing, his authority is never in question. He is known for his direct and decisive approach when needed, earning the respect of his colleagues. His ability to navigate the complexities of the media industry while maintaining a positive work environment is truly remarkable.</p>
+                            <p style="font-weight: 300;">Parmanand Khetan is a respected business leader, frequently sought after for his media expertise. He leads by example, consistently exceeding expectations and inspiring his team to do the same. While approachable and easygoing, his authority is never in question. He is known for his direct and decisive approach when needed, earning the respect of his colleagues. His ability to navigate the complexities of the media industry while maintaining a positive work environment is truly remarkable.</p>
 
                             </div>
                         </main>
