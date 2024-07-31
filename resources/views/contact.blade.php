8b0c314cd4ff827237050378f2f3fba8e82efa18
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-07-31 08:57:05 +0530

    fix

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,7 +8,7 @@
                         <main id="main" class="site-main" style="transform: none;">
                             <div class="cm_post_page_lay_wrap" style="transform: none; margin-top:20px;">
                             <h5>Contact Us</h5>
-                            <p>For any questions, comments, suggestions, feedback or advertisements, please contact us at info@kmcliv.com.</p>
+                            <p>For any questions, comments, suggestions, feedback or advertisements, please contact us at <a href="mailto:info@kmcliv.com" target="_top">info@kmcliv.com</a>.</p>
                             <p>Address: D-4 1st Floor, Sector 10, Noida, Uttar Pradesh 201301</p>
                             <p>Contact Number: <a href="tel:08076727261">08076727261</a></p>
 
