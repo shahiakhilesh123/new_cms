40d27583996943c24e6c75f3f693296d66a3ec23
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-08-01 06:02:00 +0530

    fix

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -66,7 +66,7 @@ class FileController extends Controller
                 "full_path" => public_path('file'),
             ]
         );
-        $request->file->move($destinationPath,$fileName);
+        $file->move($destinationPath,$fileName);
         return response()->json(['file_id' => $file->id, 'file_name' => $fileName, 'box' => $request->box, 'success'=> true]);
     }
 }
