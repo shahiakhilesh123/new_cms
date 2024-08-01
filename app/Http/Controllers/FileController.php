c8227ae2d5f5dd8412d4b557a82621786d7c057c
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-08-01 05:55:57 +0530

    fix

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -54,6 +54,8 @@ class FileController extends Controller
         $destinationPath = public_path('file');
         $file = $request->file('file');
         $extension = $file->getClientOriginalExtension();
+        echo $extension;
+        die();
         $fileName = $request->file->getClientOriginalName();
         $fileName = str_replace(' ', '_',$fileName);
         $fileName = pathinfo($fileName, PATHINFO_FILENAME).time() . '.'. $extension;
