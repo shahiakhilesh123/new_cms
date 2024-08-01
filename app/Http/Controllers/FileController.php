46bc2be219a3675a952523cefa9b34bdaa8414f9
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-08-01 06:31:10 +0530

    fix

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -71,6 +71,7 @@ class FileController extends Controller
         //         "full_path" => public_path('file'),
         //     ]
         // );
+        echo $fileName;
         $file->storeAs('file', $fileName, 'public');
         die();
         //return response()->json(['file_id' => $file_data->id, 'file_name' => $fileName, 'box' => $request->box, 'success'=> true]);
