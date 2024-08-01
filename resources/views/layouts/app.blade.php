fc03feeedb7614c470816a1ad28df472ab425251
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-08-01 12:58:58 +0530

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -561,7 +561,7 @@
                     <li class="home-btn"><a href="{{ asset('/news') }}">न्यूज</a></li>
                     <li class="home-btn"><a href="{{ asset('/dharma-gyan') }}">धर्म ज्ञान</a></li>
                     <!-- <li class="home-btn"><a href="{{ asset('/exclusive') }}">एक्सक्लूसिव</a></li> -->
-                    <li class="home-btn"><a href="{{ asset('/program') }}">कड़क बात </a></li>
+                    <!-- <li class="home-btn"><a href="{{ asset('/program') }}">कड़क बात </a></li> -->
                     <li>
                     <a href="{{ asset('/Podcast') }}" aria-current="page">
                        <img src="{{ asset('/file') }}/Website_Button_Podcast1722477534.jpg"> 
