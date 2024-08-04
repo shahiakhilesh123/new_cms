b101ecca9cbb2cc50354cdfcc95fa5975663751a
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-08-04 14:53:18 +0530

    fix

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -239,7 +239,7 @@
                                                         $podcast =  App\Models\Blog::where('status', '1')->whereIn('categories_ids',array(23))->orderBy('id', 'DESC')->first();
                                                         ?>
                                                         <!-- SquareAd_1_Responsitve -->
-                                                        <iframe class="attachment-full size-full wp-post-image" width="320px" height="250px" src="{{ $podcast->link}}"></iframe>
+                                                        <iframe class="attachment-full size-full wp-post-image" width="320px" height="250px" src="{{ $podcast->link}}" autoplay controls muted></iframe>
                                                         <p><h5 style="margin-left: 11px;">{{ $podcast->name }}</h5></p>
                                                 </div>
                                                     <div id="media_image-2" class="widget widget_media_image">
