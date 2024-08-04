d90251ae0ac19e5142c45d3d73e1a2933f50b1c2
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-08-04 18:40:30 +0530

    fix

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -26,6 +26,20 @@
                     <div id="primary" class="content-area" style="transform: none;">
                         <main id="main" class="site-main" style="transform: none;">
                             <div class="cm_post_page_lay_wrap" style="transform: none;">
+                                <div class="row">
+                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3986924419662120"
+                                    crossorigin="anonymous"></script>
+                                <!-- HeaderResponsive_728 -->
+                                <ins class="adsbygoogle"
+                                    style="display:block"
+                                    data-ad-client="ca-pub-3986924419662120"
+                                    data-ad-slot="5487378089"
+                                    data-ad-format="auto"
+                                    data-full-width-responsive="true"></ins>
+                                <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                </script>
+                                </div>
                                 <div class="breadcrumb  default-breadcrumb" style="display: block;">
                                     <nav role="navigation" aria-label="Breadcrumbs" class="breadcrumb-trail breadcrumbs"
                                         itemprop="breadcrumb">
