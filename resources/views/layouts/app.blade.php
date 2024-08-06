ff1e1708bd142297eccf577cbcba56f397066247
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2024-08-06 20:09:38 +0530

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -656,20 +656,6 @@
         </div>
         <div id="content" class="site-content">
         @yield('content')
-        <div class="row">
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3986924419662120"
-            crossorigin="anonymous"></script>
-        <!-- HeaderResponsive_728 -->
-        <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-3986924419662120"
-            data-ad-slot="5487378089"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-        </div>
         </div>
         <footer class="footer">
             <div class="footer_inner">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1356,6 +1356,20 @@
                                 <?php $i++; ?>
                             @endforeach
                         </div>
+                        <div class="row">
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3986924419662120"
+                            crossorigin="anonymous"></script>
+                        <!-- HeaderResponsive_728 -->
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-3986924419662120"
+                            data-ad-slot="5487378089"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+                        </div>
                     </section>
                 </div>
             </div>
